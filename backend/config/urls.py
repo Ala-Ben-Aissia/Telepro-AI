1c0c97bfd800ef72c7e26e1ff9bb11f20a9626de
Ala Ben Aissia <xalabenaissiax@outlook.com>
2025-03-09 13:10:06 +0100

    Update Main URLs and settings Configuration (apps)

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -4,4 +4,6 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/accounts/", include("accounts.urls")),
+    path("api/patients/", include("patients.urls")),
+    path("api/campaigns/", include("campaign.urls")),
 ]
