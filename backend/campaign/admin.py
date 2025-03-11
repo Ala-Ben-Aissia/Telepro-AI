99c4360e8d4341ab40e053865b548516eb951387
Ala Ben Aissia <xalabenaissiax@outlook.com>
2025-03-11 10:42:06 +0100

    Rename app to campaign's'

--- backend/campaign/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import Campaign, CampaignCategory, CommunicationLog
-
-admin.site.register(Campaign)
-admin.site.register(CommunicationLog)
-admin.site.register(CampaignCategory)
