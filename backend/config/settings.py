345bd5b872ed75c5282bebc6f7120264824ebe0f
Ala Ben Aissia <xalabenaissiax@outlook.com>
2025-03-14 16:55:19 +0100

    Implemented API Documentation with Swagger/OpenAPI

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     # Third party apps
     "rest_framework",
     "rest_framework_simplejwt",
+    # "drf_yasg"
     # My apps
     "accounts",
     "patients",
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,9 +1,39 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from drf_yasg import openapi, views
+from rest_framework import permissions
+
+schema_view = views.get_schema_view(
+    openapi.Info(
+        title="Telepro-AI API",
+        default_version="v1",
+        description="API for patient teleprospection with AI",
+        terms_of_service="https://www.teleproai.com/terms/",
+        contact=openapi.Contact(email="contact@teleproai.com"),
+        license=openapi.License(name="Private License"),
+    ),
+    public=False,
+    permission_classes=[permissions.IsAuthenticated],
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/accounts/", include("accounts.urls")),
     path("api/patients/", include("patients.urls")),
     path("api/campaigns/", include("campaigns.urls")),
+    re_path(
+        r"^api/swagger(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    re_path(
+        r"^api/swagger/$",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    re_path(
+        r"^api/redoc/$",
+        schema_view.with_ui("redoc", cache_timeout=0),
+        name="schema-redoc",
+    ),
 ]
