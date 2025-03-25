d3ba949b4568e38c054670603bd488ca53308df1
Ala Ben Aissia <xalabenaissiax@outlook.com>
2025-03-25 20:38:40 +0100

    Added TLS and security settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -222,3 +222,23 @@ SPECTACULAR_SETTINGS = {
 }
 
 SITE_URL = os.getenv("BASE_URL", "http://localhost:8000")
+
+SECURE_SSL_REDIRECT = True
+SECURE_HSTS_SECONDS = 31536000  # 1 year
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+X_FRAME_OPTIONS = "DENY"
+
+# For SMS and email templates
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "noreply@teleproai.example.com")
+
+# Security settings
+PASSWORD_HASHERS = [
+    "django.contrib.auth.hashers.Argon2PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
+]
